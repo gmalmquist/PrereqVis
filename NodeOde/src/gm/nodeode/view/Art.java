d090f2d696f58b7841f27dd7df4387faa180ce73
Garrett Malmquist <garrett.malmquist@gmail.com>
2013-12-19 00:36:54 -0800

    Gansner already did what I'm doing years ago

--- NodeOde/src/gm/nodeode/view/Art.java
+++ NodeOde/src/gm/nodeode/view/Art.java
@@ -4,6 +4,7 @@ import gm.nodeode.math.GP;
 import gm.nodeode.math.Mathf;
 import gm.nodeode.math.Pt;
 
+import java.awt.Color;
 import java.awt.FontMetrics;
 import java.awt.Graphics2D;
 
@@ -25,15 +26,46 @@ public class Art {
 	}
 	
 	public static void arrow(Graphics2D g, GP A, GP B) {
-		line(g, A, B);
-		
+		arrow(g, A, B, false);
+	}
+	public static void arrow(Graphics2D g, GP A, GP B, boolean taperStyle) {
 		Pt rot = Pt.P(0,0).add(A).sub(B);
-		float m = rot.mag();
-		float l = 10f;//Mathf.max(10f, m/10f);
-		rot.mul(l/m);
 		
-		line(g, B, rot.d().rot2d( Mathf.PI*1/5).add(B));
-		line(g, B, rot.d().rot2d(-Mathf.PI*1/5).add(B));
+		if (taperStyle) {
+			Pt T = rot.d().normalize();
+			Pt N = T.d().rot2d();
+			float s0 = 10;
+			float s1 = 2;
+			
+			Pt[] pts = {
+				N.d().mul(+s0).add(A).add(s0, T),
+				N.d().mul(+s1).add(B),
+				N.d().mul(-s1).add(B),
+				N.d().mul(-s0).add(A).add(s0, T)
+			};
+			
+			int[] x = new int[pts.length];
+			int[] y = new int[pts.length];
+			for (int i = 0; i < pts.length; i++) {
+				x[i] = pts[i].ix();
+				y[i] = pts[i].iy();
+			}
+
+			Color c = g.getColor();
+			g.setColor(Color.GRAY);
+			g.fillPolygon(x, y, pts.length);
+			g.setColor(c);
+			g.drawPolygon(x, y, pts.length);
+		} else {
+			line(g, A, B);
+			
+			float m = rot.mag();
+			float l = 10f;//Mathf.max(10f, m/10f);
+			rot.mul(l/m);
+			
+			line(g, B, rot.d().rot2d( Mathf.PI*1/5).add(B));
+			line(g, B, rot.d().rot2d(-Mathf.PI*1/5).add(B));
+		}
 	}
 	
 	public static void show(Graphics2D g, GP P, float radius, String label) {
